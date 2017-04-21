8be71b339089707a9b5be5861a70e2711c8efb17
m-daniloff <michael.daniloff@gmail.com>
2017-04-20 21:59:32 -0400

    more changes...

---
+++ FriendStorage/FriendStorage.UI/DataProvider/FriendDataProvider.cs
@@ -0,0 +1,39 @@
+﻿using FriendStorage.DataAccess;
+using FriendStorage.Model;
+using System;
+
+namespace FriendStorage.UI.DataProvider
+{
+    public class FriendDataProvider : IFriendDataProvider
+    {
+        private Func<IDataService> _dataServiceCreator;
+
+        public FriendDataProvider(Func<IDataService> dataServiceCreator)
+        {
+            _dataServiceCreator = dataServiceCreator;
+        }
+        public void DeleteFriend(int id)
+        {
+            using (var dataService = _dataServiceCreator())
+            {
+                dataService.DeleteFriend(id);
+            }
+        }
+
+        public Friend GetFriendById(int id)
+        {
+            using (var dataService = _dataServiceCreator())
+            {
+                return dataService.GetFriendById(id);
+            }
+        }
+
+        public void SaveFriend(Friend friend)
+        {
+            using (var dataService = _dataServiceCreator())
+            {
+                dataService.SaveFriend(friend);
+            }
+        }
+    }
+}
--- FriendStorage/FriendStorage.UI/ViewModel/FriendEditViewModel.cs
+++ FriendStorage/FriendStorage.UI/ViewModel/FriendEditViewModel.cs
@@ -1,21 +1,37 @@
 ﻿using System;
 using FriendStorage.Model;
+using FriendStorage.UI.DataProvider;
 
 namespace FriendStorage.UI.ViewModel
 {
     public class FriendEditViewModel : ViewModelBase, IFriendEditViewModel
     {
+        private IFriendDataProvider _dataProvider;
+        private Friend _friend;
+
+        public FriendEditViewModel(IFriendDataProvider dataProvider)
+        {
+            this._dataProvider = dataProvider;
+        }
+
         public Friend Friend
         {
             get
             {
-                throw new NotImplementedException();
+                return _friend;
+            }
+
+            private set
+            {
+                _friend = value;
+                OnPropertyChanged();
             }
         }
 
         public void Load(int friendId)
         {
-            throw new NotImplementedException();
+            _friend = _dataProvider.GetFriendById(friendId);
+            Friend = _friend;
         }
     }
 }
